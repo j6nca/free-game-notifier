6d3bb6c9d554aec720d7417477b24f4a1c081fff
jonathan.ng <j6n.work@gmail.com>
2024-04-19 01:42:38 -0400

    fix redirect

--- site/src/app/redirect/page.tsx
+++ site/src/app/redirect/page.tsx
@@ -1,21 +1,19 @@
 'use client'
 
-import { useSearchParams } from 'next/navigation'
+import { redirect, useSearchParams } from 'next/navigation'
 import { Suspense } from 'react'
 
 function Search() {
   const searchParams = useSearchParams()
   const game = searchParams.get('game')
-  const urlPrefix = "0; URL=com.epicgames.launcher://store"
+  const urlPrefix = "com.epicgames.launcher://store"
   var content = game == null ? urlPrefix : urlPrefix + "/p/" + game
  
-  return <meta http-equiv="refresh" content={content}/>
+  redirect(content)
 }
 
 export default function Page() {
-  return <head className='redirect'>
-    <Suspense>
-      <Search/>
-    </Suspense>
-  </head>
+  return <Suspense>
+    <Search/>
+  </Suspense>
 }
