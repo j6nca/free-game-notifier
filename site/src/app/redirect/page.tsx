155e6223fd3458f7c11165c650e535f25e107839
jonathan.ng <j6n.work@gmail.com>
2024-04-18 23:23:32 -0400

    throw search params into function

--- site/src/app/redirect/page.tsx
+++ site/src/app/redirect/page.tsx
@@ -3,15 +3,20 @@
 import { useSearchParams } from 'next/navigation'
 import { Suspense } from 'react'
 
-export default function Page() {
+function Search() {
   const searchParams = useSearchParams()
   const game = searchParams.get('game')
   const urlPrefix = "0; URL=com.epicgames.launcher://store"
   var content = game == null ? urlPrefix : urlPrefix + "/p/" + game
-  return <Suspense>
-    <head className='redirect'>
+ 
+  return <head className='redirect'>
       <meta http-equiv="refresh" content={content}/>
       redirecting to desktop app ...
     </head>
+}
+
+export default function Page() {
+  return <Suspense>
+    <Search/>
   </Suspense>
 }
