6a798f0e3f2c7731d66ff80e33ae3aa75fb110f9
jonathan.ng <j6n.work@gmail.com>
2024-04-18 23:21:42 -0400

    add suspense wrapper

--- site/src/app/redirect/page.tsx
+++ site/src/app/redirect/page.tsx
@@ -1,12 +1,17 @@
 'use client'
+
 import { useSearchParams } from 'next/navigation'
-export default async function Page() {
+import { Suspense } from 'react'
+
+export default function Page() {
   const searchParams = useSearchParams()
   const game = searchParams.get('game')
   const urlPrefix = "0; URL=com.epicgames.launcher://store"
   var content = game == null ? urlPrefix : urlPrefix + "/p/" + game
-  return <head className='redirect'>
-    <meta http-equiv="refresh" content={content}/>
-    redirecting to desktop app ...
-  </head>
+  return <Suspense>
+    <head className='redirect'>
+      <meta http-equiv="refresh" content={content}/>
+      redirecting to desktop app ...
+    </head>
+  </Suspense>
 }
